52f9b91a6530237436bc3ed6d4a5b7d761f6e746
ctyar <ctyar@outlook.com>
2023-04-30 19:09:50 +0100

    Fix the elevation issue with gsudo

--- dns.ps1
+++ dns.ps1
@@ -1,11 +1,1 @@
-$interfaceAlias = (Get-NetAdapter | where InterfaceDescription -eq "Killer Wireless-n/a/ac 1535 Wireless Network Adapter")."Name"
-
-$ip = (Get-NetIPConfiguration | where InterfaceAlias -eq $interfaceAlias).IPv4Address.IPAddress
-
-Set-DNSClientServerAddress $interfaceAlias –ServerAddresses ($ip)
-
-cd C:\Shahriar\Tools\DnsServerPortable\
-
-Start-Process -Wait dotnet DnsServerApp.dll 
-
-Set-DNSClientServerAddress $interfaceAlias -ResetServerAddresses
+Start-Process powershell.exe -Verb RunAs -ArgumentList "-File C:\Shahriar\Projects\PowerCogs\dnsCore.ps1"
