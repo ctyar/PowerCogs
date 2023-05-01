804cdded9d4c41280e8979a33c0976f33243bbcd
ctyar <ctyar@outlook.com>
2023-05-01 17:45:00 +0100

    Fix the ps1 double click issue

--- dns.ps1
+++ dns.ps1
@@ -1,1 +1,11 @@
-Start-Process powershell.exe -Verb RunAs -ArgumentList "-File C:\Shahriar\Projects\PowerCogs\dnsCore.ps1"
+$interfaceAlias = (Get-NetAdapter | where InterfaceDescription -eq "Killer Wireless-n/a/ac 1535 Wireless Network Adapter")."Name"
+
+$ip = (Get-NetIPConfiguration | where InterfaceAlias -eq $interfaceAlias).IPv4Address.IPAddress
+
+gsudo Set-DNSClientServerAddress $interfaceAlias –ServerAddresses ($ip)
+
+cd C:\Shahriar\Tools\DnsServerPortable\
+
+Start-Process -Wait dotnet DnsServerApp.dll 
+
+gsudo Set-DNSClientServerAddress $interfaceAlias -ResetServerAddresses
